0c0d42ec6266877f1e6405896491e5d86df49bbb
mbsmpe <mbsmpe@gmail.com>
2024-10-15 10:18:43 +0300

    commit 3.0

---
+++ task4/primes.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Заполняет массив индикаторов простых чисел от 0 до n включительно,
+   primes — массив размера не менее n + 1. */
+void calculate_primes(int primes[], int n);
+
+int main() {
+    
+}
